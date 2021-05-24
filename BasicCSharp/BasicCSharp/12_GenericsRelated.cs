4e1eee11c9ed7110c65d47f9a699e737a4fb597d
XinguoWang-TW <xinguo.wang@thoughtworks.com>
2021-05-24 14:55:29 +0800

    Finish 12_GenericsRelated unit test

--- BasicCSharp/BasicCSharp/12_GenericsRelated.cs
+++ BasicCSharp/BasicCSharp/12_GenericsRelated.cs
@@ -15,7 +15,7 @@ namespace BasicCSharp
             Type valuePropertyType = genericObjectWithStringAsTypeArgument.GetPropertyType("Value");
 
             // correct the variable value to fix the test
-            Type expectedValuePropertyType = typeof(object);
+            Type expectedValuePropertyType = typeof(string);
 
             Assert.Equal(expectedValuePropertyType, valuePropertyType);
         }
@@ -26,7 +26,7 @@ namespace BasicCSharp
             var demoObject = new ValueTypeRestrictedGenericDemoClass<int>();
 
             // correct the variable value to fix the test
-            const int expectedInitialValue = 1;
+            const int expectedInitialValue = 0;
 
             Assert.Equal(expectedInitialValue, demoObject.Value);
         }
@@ -37,7 +37,7 @@ namespace BasicCSharp
             var demoObject = new RefTypeRestrictedGenericDemoClass<string>();
 
             // correct the variable value to fix the test
-            const string expectedInitialValue = "Hello";
+            const string expectedInitialValue = null;
 
             Assert.Equal(expectedInitialValue, demoObject.Value);
         }
@@ -48,7 +48,7 @@ namespace BasicCSharp
             var demoObject = new DefaultCtorRestrictedGenericDemoClass<SayHelloByDefault>();
 
             // correct the variable value to fix the test
-            const string expectedStringValue = default(string);
+            const string expectedStringValue = "Hello";
 
             Assert.Equal(expectedStringValue, demoObject.Value.ToString());
         }
@@ -59,7 +59,7 @@ namespace BasicCSharp
             var demoObject = new InterfaceRestrictedGenericDemoClass<Duck>();
 
             // correct the variable value to fix the test
-            const string expectedStringValue = "";
+            const string expectedStringValue = "Ga, ga, ...";
 
             Assert.Equal(expectedStringValue, demoObject.ToString());
         }
@@ -70,7 +70,7 @@ namespace BasicCSharp
             string actualReturnValue = GenericMethodDemoClass.ResolvableGenericMethod(new Duck());
 
             // correct the variable value to fix the test
-            const string expectedReturnValue = "";
+            const string expectedReturnValue = "ResolvableGenericMethod(T) called. T is Duck";
 
             Assert.Equal(expectedReturnValue, actualReturnValue);
         }
@@ -81,7 +81,7 @@ namespace BasicCSharp
             string actualReturnValue = GenericMethodDemoClass.NotResolvableGenericMethod<string>();
 
             // correct the variable value to fix the test
-            const string expectedReturnValue = "";
+            const string expectedReturnValue = "NotResolvableGenericMethod() called. T is String";
 
             Assert.Equal(expectedReturnValue, actualReturnValue);
         }
@@ -90,7 +90,7 @@ namespace BasicCSharp
         public void should_be_different_types_for_different_type_argument()
         {
             // correct the variable value to fix the test
-            const bool areEqual = true;
+            const bool areEqual = false;
 
             Assert.Equal(
                 areEqual,
@@ -105,8 +105,8 @@ namespace BasicCSharp
             GenericTypeStaticDataDemoClass<int>.Count = 2;
 
             // correct the variable values for the following 2 lines to fix the test
-            const int expectedCountForIntClosedType = 0;
-            const int expectedCountForStringClosedType = 0;
+            const int expectedCountForIntClosedType = 2;
+            const int expectedCountForStringClosedType = 5;
 
             Assert.Equal(expectedCountForIntClosedType, GenericTypeStaticDataDemoClass<int>.Count);
             Assert.Equal(expectedCountForStringClosedType, GenericTypeStaticDataDemoClass<string>.Count);
@@ -120,7 +120,7 @@ namespace BasicCSharp
             object value = covariantWithBaseTypeArgument.Get();
 
             // correct the variable value to fix the test
-            object expectedValue = null;
+            object expectedValue = "Hello";
 
             Assert.Equal(expectedValue, value);
         }
@@ -134,7 +134,7 @@ namespace BasicCSharp
             contravariantWithDerivedTypeArgument.Put("Hello");
 
             // correct the variable value to fix the test
-            object expectedValue = null;
+            object expectedValue = "Hello";
 
             Assert.Equal(expectedValue, contravariantDemoObject.Get());
         }
