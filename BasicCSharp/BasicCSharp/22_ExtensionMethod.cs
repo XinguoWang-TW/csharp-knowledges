4e5152fa84071fc17d6122f05f68904cfaa91239
XinguoWang-TW <xinguo.wang@thoughtworks.com>
2021-05-24 15:04:42 +0800

    Finish 22_ExtensionMethod unit test

--- BasicCSharp/BasicCSharp/22_ExtensionMethod.cs
+++ BasicCSharp/BasicCSharp/22_ExtensionMethod.cs
@@ -16,7 +16,7 @@ namespace BasicCSharp
             string returnValue = message.OhGodItLooksAsIfIWasAMemberOfString();
 
             // please update variable value to fix the test.
-            const string expectedReturnValue = "";
+            const string expectedReturnValue = "Hello!";
 
             Assert.Equal(expectedReturnValue, returnValue);
         }
@@ -30,7 +30,7 @@ namespace BasicCSharp
                 numberList.MySelect(number => number.ToString(CultureInfo.InvariantCulture));
 
             // please update variable value to fix the test.
-            IEnumerable<string> expectedResult = new[] {"hehe"};
+            IEnumerable<string> expectedResult = new[] {"1","2","3"};
 
             Assert.Equal(expectedResult, transformedResult);
         }
