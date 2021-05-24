72b71807dd98b60c01a90a6b2335a9eb4ce99463
XinguoWang-TW <xinguo.wang@thoughtworks.com>
2021-05-24 14:54:07 +0800

    Finish 11_Enums unit test

--- BasicCSharp/BasicCSharp/11_Enums.cs
+++ BasicCSharp/BasicCSharp/11_Enums.cs
@@ -15,8 +15,8 @@ namespace BasicCSharp
             #pragma warning restore 1718
 
             // change the variable value for following 2 lines to fix the test.
-            const bool expectedResultForBottomEqualsLeft = true;
-            const bool expectedResultForRightEqualsRight = false;
+            const bool expectedResultForBottomEqualsLeft = false;
+            const bool expectedResultForRightEqualsRight = true;
 
             Assert.Equal(expectedResultForBottomEqualsLeft, bottomEqualsLeft);
             Assert.Equal(expectedResultForRightEqualsRight, rightEqualsRight);
@@ -26,10 +26,10 @@ namespace BasicCSharp
         public void should_increase_the_integer_automatically()
         {
             // change the variable value for following 4 lines to fix the test.
-            const int leftValue = int.MinValue;
-            const int rightValue = int.MinValue;
-            const int topValue = int.MinValue;
-            const int bottomValue = int.MinValue;
+            const int leftValue = 0;
+            const int rightValue = 1;
+            const int topValue = 2;
+            const int bottomValue = 3;
 
             Assert.True((BorderSide) leftValue == BorderSide.Left);
             Assert.True((BorderSide) rightValue == BorderSide.Right);
@@ -41,10 +41,10 @@ namespace BasicCSharp
         public void should_specify_explicity_integer_value_for_each_member()
         {
             // change the variable value for following 4 lines to fix the test.
-            const int leftValue = int.MinValue;
-            const int rightValue = int.MinValue;
-            const int topValue = int.MinValue;
-            const int bottomValue = int.MinValue;
+            const int leftValue = 12;
+            const int rightValue = 34;
+            const int topValue = 56;
+            const int bottomValue = 78;
 
             Assert.True((BorderSideExplicity)leftValue == BorderSideExplicity.Left);
             Assert.True((BorderSideExplicity)rightValue == BorderSideExplicity.Right);
@@ -59,7 +59,7 @@ namespace BasicCSharp
                 BorderSideExplicity.Left == BorderSideExplicity.LeftEquivalent;
 
             // change the variable value to fix the test.
-            const bool expectedCompareResult = false;
+            const bool expectedCompareResult = true;
 
             Assert.Equal(expectedCompareResult, differentDeclareWithSameValueCompareResult);
         }
@@ -68,10 +68,10 @@ namespace BasicCSharp
         public void should_increase_the_integer_according_to_layout()
         {
             // change the variable value for following 4 lines to fix the test.
-            const int leftValue = int.MinValue;
-            const int rightValue = int.MinValue;
-            const int topValue = int.MinValue;
-            const int bottomValue = int.MinValue;
+            const int leftValue = 12;
+            const int rightValue = 13;
+            const int topValue = 78;
+            const int bottomValue = 79;
 
             Assert.True((BorderSideLayout)leftValue == BorderSideLayout.Left);
             Assert.True((BorderSideLayout)rightValue == BorderSideLayout.Right);
@@ -85,7 +85,7 @@ namespace BasicCSharp
             var parsedBottomEnumValue = (BorderSide)Enum.Parse(typeof(BorderSide), "Bottom");
 
             // change the variable value to fix the test.
-            const BorderSide expectedEnumValue = BorderSide.Left;
+            const BorderSide expectedEnumValue = BorderSide.Bottom;
 
             Assert.Equal(expectedEnumValue, parsedBottomEnumValue);
         }
@@ -96,7 +96,7 @@ namespace BasicCSharp
             var parsedBottomEnumValue = (BorderSide)Enum.Parse(typeof(BorderSide), "3");
 
             // change the variable value to fix the test.
-            const BorderSide expectedEnumValue = BorderSide.Left;
+            const BorderSide expectedEnumValue = BorderSide.Bottom;
 
             Assert.Equal(expectedEnumValue, parsedBottomEnumValue);
         }
@@ -109,7 +109,7 @@ namespace BasicCSharp
             const bool includeLeft = (leftAndRight & BorderSideFlag.Left) != 0;
             
             // change the variable value to fix the test.
-            const bool expectedIncludeLeft = false;
+            const bool expectedIncludeLeft = true;
 
             Assert.Equal(expectedIncludeLeft, includeLeft);
         }
