564148ba4e49fc4255406226ca5aa22c20eb13e0
xinguo.wang <xinguo.wang@thoughtworks.com>
2021-05-25 22:04:32 +0800

    1. To modify actions in controller when upgrading asp.net mvc to Microsoft.AspNetCore.Mvc. 2. To modify the view code to fit aspnetcore. 3. To install EntityFramwork core package and create database at local. 4. To upgrade xunit test package and modify the error test case.

---
+++ BasicASP.NETCoreMvcKnowledge/Controllers/AuthController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using BasicASP.NETMvc.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Session;
+
+namespace BasicASP.NETMvc.Controllers
+{
+    public class AuthController : Controller
+    {
+        [AllowAnonymous]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public ActionResult Login(User user)
+        {
+            if (user == null || !"admin".Equals(user.UserName) || !"admin".Equals(user.PassWord))
+            {
+                ViewBag.Error = "UserName and PassWord is admin";
+                return View();
+            }
+
+            CreateAuthCookie(user.UserName);
+            AddValusToSession(user.UserName);
+            return RedirectToAction("Page");
+        }
+
+
+        //basic points 14 please make sure this action should be authed.
+        [Authorize]
+        public ActionResult Page()
+        {
+            // # homework 1 -- redirect to movies/index
+            ViewBag.UserNameSession = HttpContext.Session.GetString("userName");
+            return RedirectToAction("Index","Movies");
+        }
+
+        private void CreateAuthCookie(string userName)
+        {
+            //basic points 16 please add param into Cookie 
+            // use cookie auth
+            Response.Cookies.Append("auth", userName, new CookieOptions
+            {
+                Expires = DateTime.Now.AddMinutes(30)
+            });
+        }
+
+        private void AddValusToSession(string userName)
+        {
+            //basic points 17 Add param into Session and Seeeion key is "userName"
+            HttpContext.Session.SetString("userName", userName);
+        }
+    }
+}
---
+++ BasicASP.NETCoreMvcKnowledge/Controllers/RouteController.cs
@@ -0,0 +1,26 @@
+﻿
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BasicASP.NETMvc.Controllers
+{
+    [AllowAnonymous]
+    //basic points 12 Please change "api" to made this controller root path is "api/route"
+    [Route("api/route")]
+    public class RouteController : Controller
+    {
+        // Get
+        //basic points 13 Change "xxx" and requet "/api/route/index" by Get
+        [Route("index")]
+        public string Get()
+        {
+            return "hello world";
+        }
+
+        [Route("index2")]
+        public string Post()
+        {
+            return "hello world";
+        }
+    }
+}
